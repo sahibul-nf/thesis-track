ea3a64b1b74237b34fed0242c879cb3a3297b4f2
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2025-02-24 00:39:27 +0700

    Remove thesis track Flutter app project files
    This commit removes all Flutter app project files for the thesis tracking application, cleaning up the repository by deleting unnecessary client-side code.

--- thesis_track_app/lib/features/document/domain/repository/document_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:io';
-
-abstract class DocumentRepository {
-  Future<String> uploadDraftDocument(String thesisId, File document);
-  Future<String> uploadFinalDocument(String thesisId, File document);
-}
